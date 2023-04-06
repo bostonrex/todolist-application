7c03b02bd705a3762412f61c9da4c0bee5c53fe9
Alexandra Alvarez <alexandraalvarez@MacBook-Air-de-Alexandra.local>
2023-04-06 15:38:14 -0400

    muestra numero 1

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,23 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const urlBase = "http://assets.breatheco.de/apis/fake/todos/user/";
+const apiUsername = "bostonrex";
 
 const TodoList = () => {
     const [tasks, setTasks] = useState([]);
     const [inputValue, setInputValue] = useState('');
 
-    const addTask = (e) => {
-        e.preventDefault();
-        if (inputValue.trim()) {
-            setTasks([...tasks, { text: inputValue, completed: false }]);
-            setInputValue('');
+    const fetchTodoApi = async () => {
+        try {
+            const response = await fetch(`${urlBase}${apiUsername}`);
+            const data = await response.json();
+            console.log(data);
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
         }
     };
 
-    const removeTask = (index) => {
-        const newTasks = tasks.filter((_, i) => i !== index);
-        setTasks(newTasks);
+    const syncTasks = async () => {
+        try {
+            const response = await fetch(`${urlBase}${apiUsername}`, {
+                method: 'PUT',
+                body: JSON.stringify(tasks),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Error updating tasks');
+            }
+            fetchTodoApi();
+
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    const pendingTasks = tasks.filter((task) => !task.completed).length;
+    const addTask = async (e) => {
+        e.preventDefault();
+        if (inputValue.trim()) {
+            const newTasks = [...tasks, { label: inputValue, done: false }];
+            setTasks(newTasks);
+            setInputValue('');
+            await syncTasks(newTasks);
+        }
+    };
+
+    const removeTask = async (index) => {
+        const newTasks = tasks.filter((_, i) => i !== index);
+        setTasks(newTasks);
+        await syncTasks(newTasks);
+    };
+
+    const removeAllTasks = async () => {
+        setTasks([]);
+        await syncTasks([]);
+    };
+
+    const pendingTasks = tasks.filter((task) => !task.done).length;
+
+    useEffect(() => {
+        fetchTodoApi();
+    }, []);
 
     return (
         <div className='d-flex justify-content-center mt-5'>
@@ -31,17 +76,16 @@ const TodoList = () => {
                             onChange={(e) => setInputValue(e.target.value)}
                             placeholder="Enter a task"
                         />
-
                     </form>
                     <ul>
                         {tasks.map((task, index) => (
-                            <li key={index} className={task.completed ? 'completed' : ''}>
-
-                                <span>{task.text}</span>
+                            <li key={index} className={task.done ? 'completed' : ''}>
+                                <span>{task.label}</span>
                                 <button onClick={() => removeTask(index)}>X</button>
                             </li>
                         ))}
                     </ul>
+                    <button onClick={removeAllTasks}>Clear All Tasks</button>
                 </div>
                 <div className='border'>
                     <p className='footer ms-4 mt-3'>{pendingTasks} Pending Task </p>
